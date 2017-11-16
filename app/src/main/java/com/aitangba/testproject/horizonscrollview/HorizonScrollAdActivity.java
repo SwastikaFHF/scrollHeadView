5fe411783967b4dc8426026ee8d72c04064f29ce
fenghaifeng <fhf11991@LY.com>
2017-11-16 16:10:41 +0800

    循坏播放的ViewPager

--- app/src/main/java/com/aitangba/testproject/horizonscrollview/HorizonScrollAdActivity.java
+++ app/src/main/java/com/aitangba/testproject/horizonscrollview/HorizonScrollAdActivity.java
@@ -2,6 +2,9 @@ package com.aitangba.testproject.horizonscrollview;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.aitangba.testproject.R;
@@ -18,16 +21,50 @@ public class HorizonScrollAdActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horizon_scroll);
 
-        SlideViewGroup slideViewGroup = findViewById(R.id.scrollView);
+//        SlideViewGroup slideViewGroup = findViewById(R.id.scrollView);
+//
+//        ImageView imageView = new ImageView(this);
+//        imageView.setBackgroundResource(R.drawable.bg_red);
+//
+//        slideViewGroup.addView(imageView);
+//
+//        imageView = new ImageView(this);
+//        imageView.setBackgroundResource(R.drawable.bg_update_common);
+//
+//        slideViewGroup.addView(imageView);
 
-        ImageView imageView = new ImageView(this);
-        imageView.setBackgroundResource(R.drawable.bg_red);
+        LoopViewPager loopViewPager = findViewById(R.id.loopViewPager);
 
-        slideViewGroup.addView(imageView);
+        loopViewPager.setAdapter(new Adapter());
+    }
 
-        imageView = new ImageView(this);
-        imageView.setBackgroundResource(R.drawable.bg_update_common);
+    private class Adapter extends PagerAdapter {
 
-        slideViewGroup.addView(imageView);
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = new ImageView(container.getContext());
+            if(position == 0) {
+                imageView.setBackgroundResource(R.drawable.bg_red);
+            } else {
+                imageView.setBackgroundResource(R.drawable.bg_update_common);
+            }
+            container.addView(imageView);
+            return imageView;
+        }
     }
 }
