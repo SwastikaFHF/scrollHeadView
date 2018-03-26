e98d2bc9b604380c168aa2709f168559a76dd937
fenghaifeng <fhf11991@LY.com>
2018-03-26 17:45:57 +0800

    优化网络请求

---
+++ app/src/main/java/com/aitangba/testproject/tracktask/HttpTask.java
@@ -0,0 +1,12 @@
+package com.aitangba.testproject.tracktask;
+
+/**
+ * Created by fhf11991 on 2018/3/26.
+ */
+
+public class HttpTask extends TrackedAsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
